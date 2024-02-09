83353d6ac4e5ca58d40d446a88b95f9ac88ecb03
Srikanth-Rajamaina <155744400+Srikanth-Rajamaina@users.noreply.github.com>
2024-02-09 23:50:20 +0530

    Add files via upload

---
+++ hallow_square_star.c
@@ -0,0 +1,24 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main() {
+  for(int i=0;i<5;i++){
+     
+          if(i==0 | i==4){{
+           for(int j=0;j<5;j++){
+               printf("* ");
+             }
+          }printf("\n");
+          }
+         else {
+              for(int j=0;j<5;j++){
+                  if(j==0 | j==4)
+                    printf("* ");
+                    else
+                    printf("  ");
+              }
+              printf("\n");
+         }
+  }
+      return 0;
+}
